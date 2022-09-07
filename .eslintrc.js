0319aca45ebf68f029d026af85d5b8ba8e5d15aa
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-09-07 14:42:25 +0100

    starting project

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+        "ecmaFeatures": {
+            "jsx": true
+          },
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+    },
+};
---
+++ src/components/pomodoro-timer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function PomodoroTimer(props): JSX.Element {
+    return <div>Hello World!</div>;
+}
